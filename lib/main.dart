cc9dded8a6402f16c49b4dc7e2ab67ea21400739
Jhabindra Aryal <learnerprak@gmail.com>
2021-05-07 11:50:04 +0545

    working with file system for reading and writing from/to file on device

--- lib/main.dart
+++ lib/main.dart
@@ -1,101 +1,82 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:path_provider/path_provider.dart';
 
-import 'dart:async';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
-import 'package:flutter/widgets.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  final Future<Database> database = openDatabase(
-      join(await getDatabasesPath(), 'person.db'), onCreate: (db, version) {
-    return db.execute(
-      "CREATE TABLE person(id INTEGER PRIMARY KEY,name TEXT,address TEXT)",
-    );
-  }, version: 1);
-  Future<void> insertPerson(Person person) async {
-    final Database db = await database;
-    db.insert('person', person.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  Future<List<Person>> person() async {
-    final Database db = await database;
-    final List<Map<String, dynamic>> maps = await db.query('person');
-    return List.generate(
-        maps.length,
-        (index) => Person(
-            id: maps[index]['id'],
-            name: maps[index]['name'],
-            address: maps[index]['address']));
-  }
-
-  var per = Person(id: 4, name: 'Mith', address: 'Thailand');
-  await insertPerson(per);
-  print(await person());
-  runApp(MyApp(listOfPerson: person()));
+void main() {
+  runApp(MaterialApp(
+    title: 'Reading and Writing Files',
+    home: FlutterDemo(storage: CounterStorage()),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  MyApp({this.listOfPerson});
-  final Future<List<Person>> listOfPerson;
+class CounterStorage {
+  //Finding the correct local path.
+  Future<String> get _localPath async {
+    final direcotry = await getApplicationDocumentsDirectory();
+    return direcotry.path;
+  }
+
+//Creating a reference to the file location.
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/counter.txt');
+  }
+
+//Reading data from file.
+  Future<int> readCounter() async {
+    try {
+      final file = await _localFile;
+      String contents = await file.readAsString();
+      return int.parse(contents);
+    } catch (e) {
+      return 0;
+    }
+  }
+
+  //Writing data to file.
+  Future<File> writeCounter(int counter) async {
+    final file = await _localFile;
+    return file.writeAsString('$counter');
+  }
+}
+
+class FlutterDemo extends StatefulWidget {
+  FlutterDemo({Key key, @required this.storage}) : super(key: key);
+  final CounterStorage storage;
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        canvasColor: Colors.green.shade100,
-      ),
-      home: MyHomePage(
-          title: 'Http Package Learning', listOfPerson: listOfPerson),
-    );
-  }
+  _FlutterDemoState createState() => _FlutterDemoState();
 }
 
-class Person {
-  final int id;
-  final String name;
-  final String address;
-  Person({this.id, this.name, this.address});
-  Map<String, dynamic> toMap() {
-    return {'id': id, 'name': name, 'address': address};
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title, this.listOfPerson}) : super(key: key);
-  final Future<List<Person>> listOfPerson;
-  final String title;
-
+class _FlutterDemoState extends State<FlutterDemo> {
+  int _counter;
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+  void initState() {
+    super.initState();
+    widget.storage.readCounter().then((int value) => setState(() {
+          _counter = value;
+        }));
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
+  Future<File> _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+    return widget.storage.writeCounter(_counter);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Http Fetch'),
+      body: Center(
+        child: Text('Button tapped $_counter time${_counter == 1 ? '' : 's'}.'),
       ),
-      body: FutureBuilder(
-        future: widget.listOfPerson,
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  return Text(snapshot.data[index].name);
-                });
-          } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
-          }
-          return CircularProgressIndicator();
-        },
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
       ),
     );
   }
